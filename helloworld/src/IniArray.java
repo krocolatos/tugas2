1d09d6b6cd5548cc342e5a945a3631cfa9fa3bdf
krocolatos <93392835+krocolatos@users.noreply.github.com>
2021-11-08 18:43:35 +0700

    Delete helloworld directory

--- helloworld/src/IniArray.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays; // menampilkan semua nilai array
-public class IniArray {
-    public static void main(String[] args) {
-
-        String[]
-        nama = {"andi", "budi", "andi"};
-        System.out.println(Arrays.toString(nama)); //sintaks tampilkan data array
-    }
-}
