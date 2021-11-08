40b7b0fe8117e17ac854a570db632f46a1df1fdb
krocolatos <93392835+krocolatos@users.noreply.github.com>
2021-11-08 18:42:30 +0700

    Delete Tabungan directory

--- Tabungan/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Tabungan tbg = new tbg();
-        tbg.setNama ("Jayadi");
-        System.out.println("Nama " +tbg.getNama() );
-
-
-}
